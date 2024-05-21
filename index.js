4a8f4869a211881256d7b1d99834fcc4f03ff8b9
Cauê Gallizzi <cauegallizzi397@gmail.com>
2024-05-21 15:28:14 -0300

    finished exercise

--- index.js
+++ index.js
@@ -1,37 +1,20 @@
-const form = document.getElementById('form-numbers')
-const fieldA = document.querySelector('#field-a')
-const fieldB = document.querySelector('#field-b')
-let validForm = false
+$(document).ready(() => {
+    $('.add-task').click(ev => {
+        ev.preventDefault()
 
-function validateNumbers(numberFieldA, numberFieldB) {
-    const numberA = parseInt(numberFieldA)
-    const numberB = parseInt(numberFieldB)
- 
-    return numberB > numberA ? true : false
-}
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    const messageContainer = document.querySelector('.message')
-    const successMessage = `O campo B possui o número maior que o campo A. Válido.` 
-    const errorMessage = `O campo B não possui um número maior que o campo A. Inválido.`
-    
-    validForm = validateNumbers(fieldA.value, fieldB.value)
-    if (validForm) {
-        messageContainer.classList.remove('error-message')
-        messageContainer.classList.add('success-message')
-        messageContainer.textContent = successMessage
-        messageContainer.style.display = 'block'
-        fieldA.classList.remove('error-input')
-        fieldB.classList.remove('error-input')
-        form.reset()
-    } else {
-        messageContainer.classList.remove('success-message')
-        messageContainer.classList.add('error-message')
-        messageContainer.textContent = errorMessage
-        messageContainer.style.display = 'block'
-        fieldA.classList.add('error-input')
-        fieldB.classList.add('error-input')
-    }
+        const taskList = $('#tasks')
+        const task = $('#task-name').val()
+        const newTask = $(`<li style="cursor: pointer">${task}</li>`)
+        $(newTask).click(() => {
+            if ($(newTask).is(".done")) {
+                console.log('tem done')
+                $(newTask).removeClass('done')
+            } else {
+                $(newTask).addClass('done')
+                console.log(newTask)
+            }
+        })
+        $(newTask).appendTo(taskList)
+        $(task).val('')
+    })
 })
